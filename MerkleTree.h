456558b40d27e2bcc583cd0269c66812a114d44a
Munalen <135845261+Munalen@users.noreply.github.com>
2023-07-20 14:25:32 -0400

    Create MerkleTree.h

---
+++ MerkleTree.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <hash.h>
+#include <bye.c>
+//step1 gather all c files, everything into single folder, open vs code,close everthiing, click open folder, find folder, 
+
+// scan file,make arguemnt, then construct merkle tree 
+int main()
+{
+    FILE *pF= fopen("uthash.h","r");  //open the file, read 
+    char buffer[200];  //buffer reads each line 
+
+    if(pF==NULL)
+    {
+        printf ("Unable to open file!");
+    }
+}
+struct Merkletree{
+    
+    node *curNode = 
+    while (curNode.size() >= 2) {
+    construct MerkleTreeNode*parentNodes = Merkletree
+    struct MerkleTreeNode* createMerkleNode(/* arguments for data/hashing */) {
+    struct MerkleTreeNode* newNode = (struct MerkleTreeNode*)malloc(sizeof(struct MerkleTreeNode));
+    if (newNode != NULL) {
+        // Initialize the node's 
+        newNode->left = NULL;
+        newNode->right = NULL;
+    }
+    return newNode;
+}
+    }
+
+
+
+    // add nodes and create parents 
+    while (curNode != NULL)   // do get methad after structure merkle tree 
+{
+  prevNode = curNode;
+  curNode = curNode->next;
+}
+parentsnode.add(node)
+
+{
+    curNode==parentnode;
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
